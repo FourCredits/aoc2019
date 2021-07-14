718a8630171f6c65cf4c5acda5ba23eef754f676
Erik <erik.grundy@gmail.com>
2021-07-14 16:11:34 +1200

    Make core/-main dispatch to other days' -main function

--- src/aoc2019/core.clj
+++ src/aoc2019/core.clj
@@ -3,6 +3,9 @@
   (:require [aoc2019.day-1 :as d1]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Dispatch to the day to run"
+  ([day & args]
+   (case (read-string day)
+     1 (d1/-main args)
+     (println "ERROR: Can't do that day")))
+  ([] (println "ERROR: Please specify a day")))
