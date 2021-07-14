2842c4291e8673e3d9a182bfa8f0dbf1bd30f2c7
Erik <erik.grundy@gmail.com>
2021-07-14 15:47:51 +1200

    Remove dummy test made by leiningen

--- test/aoc2019/core_test.clj
+++ test/aoc2019/core_test.clj
@@ -1,10 +1,3 @@
 (ns aoc2019.core-test
   (:require [clojure.test :refer :all]
             [aoc2019.core :refer :all]))
-
-(deftest part-1-test
-  (testing "part 1 works"
-    (is (= (part-1 12) 2))
-    (is (= (part-1 14) 2))
-    (is (= (part-1 1969) 654))
-    (is (= (part-1 100756) 33583))))
