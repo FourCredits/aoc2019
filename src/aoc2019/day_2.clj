c29b9fd2199f84ae4eb755cd33edd49793c9e2b8
Erik <erik.grundy@gmail.com>
2021-07-15 11:53:15 +1200

    Move intcode things into their own file (in preparation for exansion of the intcode machine)

--- src/aoc2019/day_2.clj
+++ src/aoc2019/day_2.clj
@@ -1,34 +1,7 @@
 (ns aoc2019.day-2
   (:gen-class)
-  (:require [clojure.string :as str]))
-
-(defn binary-op [program pc f]
-  "An abstraction of a binary operation on a program. Reads a number from
-  address (pc + 1), another from (pc + 2), performs the given binary operation
-  on them, and then writes the result to address (pc + 3)"
-  (let [target-1   (program (program (+ pc 1)))
-        target-2   (program (program (+ pc 2)))
-        source-pos (program (+ pc 3))]
-     (assoc program source-pos (f target-1 target-2))))
-
-(defn run-intcode
-  "The Intcode interpreter - takes a sequence of integers as input, and spits
-  out the completed program state"
-  ([program] (run-intcode program 0))
-  ([program pc]
-   (let [opcode (nth program pc)]
-     (case opcode
-       1  (recur (binary-op program pc +) (+ pc 4))
-       2  (recur (binary-op program pc *) (+ pc 4))
-       99 program
-       :error))))
-
-(defn get-input
-  "Read the input from the given file."
-  []
-  (mapv
-   #(Integer/valueOf %)
-   (str/split (str/trim (slurp "resources/day2.txt")) #",")))
+  (:require [clojure.string :as str]
+            [aoc2019.intcode :refer :all]))
 
 (defn part-1
   "Solves part 1 of day 2"
--- test/aoc2019/day_2_test.clj
+++ test/aoc2019/day_2_test.clj
@@ -1,20 +1,7 @@
 (ns aoc2019.day-2-test
   (:require [clojure.test :refer :all]
-            [aoc2019.day-2 :refer :all]))
-
-(deftest basic-int-code-tests
-  (testing "quit"
-    (is (= (run-intcode [99 98 97]) [99 98 97])
-        "Base case: 99, followed by anything, immediately quits"))
-  (testing "add"
-    (is (= (run-intcode [1 0 0 0 99]) [2 0 0 0 99])
-        "1 + 1 = 2"))
-  (testing "multiply"
-    (is (= (run-intcode [2 3 0 3 99]) [2 3 0 6 99])
-        "3 * 2 = 6"))
-  (testing "data space"
-    (is (= (run-intcode [1 1 1 4 99 5 6 0 99]) [30 1 1 4 2 5 6 0 99])
-        "You should be able to write into the space after a 99 command")))
+            [aoc2019.day-2 :refer [part-1 part-2]]
+            [aoc2019.intcode :refer [get-input]]))
 
 (deftest part-1-real
   (testing "Part 1 of day 2"
