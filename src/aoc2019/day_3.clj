105fad72df875974e094f188d4e1a5afb780b8a8
Erik <erik.grundy@gmail.com>
2021-07-14 18:19:11 +1200

    Add tests and method stubs for day 3

---
+++ src/aoc2019/day_3.clj
@@ -0,0 +1,12 @@
+(ns aoc2019.day-3
+  (:gen-class))
+
+(defn parse-input [input] nil)
+
+(defn solve [input] nil)
+
+(defn -main
+  "Runs day 3"
+  [& args]
+  (binding [*out* *err*]
+    (println "Not implemented yet")))
