4cdf5fa38f506ce3ffe8c6d0cc67fb3317795e85
Erik <erik.grundy@gmail.com>
2021-07-14 18:18:36 +1200

    Add days 2 and 3 to main

--- src/aoc2019/core.clj
+++ src/aoc2019/core.clj
@@ -1,11 +1,15 @@
 (ns aoc2019.core
   (:gen-class)
-  (:require [aoc2019.day-1 :as d1]))
+  (:require [aoc2019.day-1 :as d1]
+            [aoc2019.day-2 :as d2]
+            [aoc2019.day-3 :as d3]))
 
 (defn -main
   "Dispatch to the day to run"
   ([day & args]
    (case (read-string day)
      1 (d1/-main args)
+     2 (d2/-main args)
+     3 (d3/-main args)
      (println "ERROR: Can't do that day")))
   ([] (println "ERROR: Please specify a day")))
