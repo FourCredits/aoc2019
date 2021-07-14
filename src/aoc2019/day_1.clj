3aa4f2f1702dec596397cd643ff689763648ba01
Erik <erik.grundy@gmail.com>
2021-07-14 16:28:03 +1200

    Add docstrings

--- src/aoc2019/day_1.clj
+++ src/aoc2019/day_1.clj
@@ -2,22 +2,36 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(defn part-1 [x] (- (int (/ x 3)) 2))
+(defn part-1 
+  "Calculates the fuel needed for a given weight x."
+  [x]
+  (- (int (/ x 3)) 2))
 
-(defn part-2 [x]
+(defn part-2
+  "Calculates the fuel needed for a given weight x, and the fuel needed for
+  that fuel, and so on, until the amount becomes less than 0. Returns the sum."
+  [x]
   (->> x
        (iterate part-1)
        (next)
        (take-while pos?)
        (apply +)))
 
-(defn solve [f nums]
+(defn solve
+  "For a given list of weights `nums`, finds the amount of fuel needed for
+  that fuel using the weight function f. Returns the sum of those amounts
+  of fuel."
+  [f nums]
   (apply + (map f nums)))
 
-(defn get-input []
+(defn get-input
+  "Reads the input from the input file."
+  []
   (map read-string (str/split (slurp "resources/day1.txt") #"\n")))
 
-(defn -main [& args]
+(defn -main
+  "Prints the result for part 1 and part 2 of day 1"
+  [& args]
   (let [input (get-input)]
     (println "Part 1:" (solve part-1 input))
     (println "Part 2:" (solve part-2 input))))
