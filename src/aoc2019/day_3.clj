ea91655d3dd2800236126dac6537453c3d7b74ba
Erik <erik.grundy@gmail.com>
2021-07-14 22:44:48 +1200

    Day 3 Part 1

--- src/aoc2019/day_3.clj
+++ src/aoc2019/day_3.clj
@@ -1,12 +1,57 @@
 (ns aoc2019.day-3
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
-(defn parse-input [input] nil)
+(defn parse-line
+  "Parses a single line"
+  [line]
+  (for [command (str/split line #",")]
+    [(first command) (read-string (subs command 1))]))
 
-(defn solve [input] nil)
+(defn parse-input
+  "Turns a string of input into a pair of vectors. Each vector is a collection
+  of pairs, where the first element is one of {:left, :right, :up, :down}, and
+  the second is a number. This represents the two wires, and the description of
+  how they move from the centre."
+  [input]
+  (let [[line1 line2] (str/split-lines input)]
+    (map parse-line [line1 line2])))
+
+(def ^:private direction-vectors
+  {\L [-1  0]
+   \R [ 1  0]
+   \U [ 0  1]
+   \D [ 0 -1]})
+
+(defn make-path
+  "Takes a list of instructions describing the path a wire takes, and returns
+  the positions that the wire passes through"
+  ([wire] (make-path wire [0 0]))
+  ([wire [x y]]
+   (if-let [[direction target] (first wire)]
+     (let [[dx dy]  (direction-vectors direction)
+           progress (map #(vector (+ x (* dx %)) (+ y (* dy %)))
+                         (range 1 (inc target)))]
+       (concat progress (make-path (rest wire) (last progress)))))))
+
+(defn manhattan-distance [[x y]]
+  (+ (Math/abs x) (Math/abs y)))
+
+(defn part-1
+  "Takes in a string of input, representing the paths of two wires, and returns
+  the manhattan distance of the nearest intersection to the point that the two
+  wires start from."
+  [input]
+  (let [[wire1 wire2] (parse-input input)
+        path1 (set (make-path wire1))
+        path2 (set (make-path wire2))]
+    (->> (set/intersection path1 path2)
+         (map manhattan-distance)
+         (sort)
+         (first))))
 
 (defn -main
   "Runs day 3"
   [& args]
-  (binding [*out* *err*]
-    (println "Not implemented yet")))
+  (println (part-1 (slurp "resources/day3.txt"))))
--- test/aoc2019/day_3_test.clj
+++ test/aoc2019/day_3_test.clj
@@ -2,16 +2,26 @@
   (:require [clojure.test :refer :all]
             [aoc2019.day-3 :refer :all]))
 
+(deftest parse-line-test
+  (testing "Real basics"
+    (is (= (parse-line "R5,D6,U7,L8")
+           [[\R 5]
+            [\D 6]
+            [\U 7]
+            [\L 8]]))))
+
 (deftest parse-test
   (testing "Basic case"
     (let [input "R8,U5,L5,D3\nU7,R6,D4,L4"]
       (is (= (parse-input input)
-             [[[:right 8] [:up 5] [:left 5] [:down 3]]
-              [[:up 7] [:right 6] [:down 4] [:left 4]]])))))
+             [[[\R 8] [\U 5] [\L 5] [\D 3]]
+              [[\U 7] [\R 6] [\D 4] [\L 4]]])))))
 
 (deftest part-1-tests
-  (testing
-   (for [[answer input] [[6   "R8,U5,L5,D3\nU7,R6,D4,L4"]
-                         [159 "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"]
-                         [135 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]]]
-     (is (= (solve input) answer)))))
+  (are [answer input] (= (solve input) answer)
+    6   "R8,U5,L5,D3\nU7,R6,D4,L4"
+    159 "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"
+    135 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"))
+
+(deftest part-1-real
+  (is (solve (slurp "resources/day3.txt")) 1195))
