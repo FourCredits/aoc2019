06c4614f03b965370a31cb5902e48c7017592931
Erik <erik.grundy@gmail.com>
2021-07-15 20:12:45 +1200

    Add base src file for easy copying

---
+++ resources/base.clj
@@ -0,0 +1,16 @@
+(ns aoc2019.day- ; TODO
+  (:gen-class))
+
+(defn part-1 [input]
+  nil)
+
+(defn part-2 [input]
+  nil)
+
+(defn get-input []
+  nil)
+
+(defn -main [& args]
+  (let [input (get-input)]
+    (println (part-1 input))
+    (println (part-2 input))))
