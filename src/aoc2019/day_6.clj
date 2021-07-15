9bdbe33de1eb520fdd512cd9c4a6f111f84ed910
Erik <erik.grundy@gmail.com>
2021-07-16 01:10:41 +1200

    Implement day 6 part 1

--- src/aoc2019/day_6.clj
+++ src/aoc2019/day_6.clj
@@ -2,13 +2,34 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(defn part-1 [orbits] nil)
+(defn find-sub-orbits
+  "Returns a list of objects that orbit `object`, taken from the `orbits`
+  collection."
+  [object orbits]
+  (map second (filter #(= object (first %)) orbits)))
 
-(defn part-2 [orbits] nil)
+(defn make-table [orbits]
+  (let [unique-orbits (set (apply concat orbits))
+        sub-orbits    (map #(find-sub-orbits % orbits) unique-orbits)
+        lookup-table  (into {} (map vector unique-orbits sub-orbits))]
+    lookup-table))
 
-(defn parse [string] nil)
+(defn part-1 [orbits]
+    ; Depth-first traversal
+    (loop [result 0
+           queue  '([0 "COM"])]
+      (if (empty? queue)
+        result
+        (let [[depth k] (peek queue)]
+          (recur (+ result depth)
+                 (apply list (concat (pop queue) (map #(vector (inc depth) %) (orbits k)))))))))
 
 (defn get-input [] nil)
+(defn parse [string]
+  (make-table (map #(str/split % #"\)") (str/split-lines string))))
+
+(defn get-input []
+  (parse (slurp "resources/day6.txt")))
 
 (defn -main [& args]
   (let [input (get-input)]
