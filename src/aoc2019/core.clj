a9415122f9d3016393a446c4991835a5b2220e6a
Erik <erik.grundy@gmail.com>
2021-07-15 20:10:45 +1200

    Refactor core to be a little simpler to run

--- src/aoc2019/core.clj
+++ src/aoc2019/core.clj
@@ -3,15 +3,22 @@
   (:require [aoc2019.day-1 :as d1]
             [aoc2019.day-2 :as d2]
             [aoc2019.day-3 :as d3]
-            [aoc2019.day-4 :as d4]))
+            [aoc2019.day-4 :as d4]
+            [aoc2019.day-5 :as d5]
+            [aoc2019.day-6 :as d6]))
+
+(def main-funcs
+  {1 d1/-main
+   2 d2/-main
+   3 d3/-main
+   4 d4/-main
+   5 d5/-main
+   6 d6/-main})
 
 (defn -main
   "Dispatch to the day to run"
   ([day & args]
-   (case (read-string day)
-     1 (d1/-main args)
-     2 (d2/-main args)
-     3 (d3/-main args)
-     4 (d4/-main args)
+   (if-let [main (get main-funcs (Integer/valueOf day))]
+     (main args)
      (println "ERROR: Can't do that day")))
   ([] (println "ERROR: Please specify a day")))
