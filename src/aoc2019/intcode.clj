dadc1da9d56480648ea7e190fc5faaa683c7e2fc
Erik <erik.grundy@gmail.com>
2021-07-15 21:56:28 +1200

    A bunch of refactorings and additions (too many to list)

--- src/aoc2019/intcode.clj
+++ src/aoc2019/intcode.clj
@@ -2,36 +2,56 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(defn binary-op [program pc f]
-  "An abstraction of a binary operation on a program. Reads a number from
-  address (pc + 1), another from (pc + 2), performs the given binary operation
-  on them, and then writes the result to address (pc + 3)"
-  (let [target-1   (program (program (+ pc 1)))
-        target-2   (program (program (+ pc 2)))
-        source-pos (program (+ pc 3))]
-     (assoc program source-pos (f target-1 target-2))))
+(defn digits
+  "Takes a number `n` and returns its digits as a list"
+  [n]
+  (loop [acc '()
+         i   n]
+    (if (zero? i)
+      acc
+      (recur (conj acc (mod i 10)) (int (/ i 10))))))
+
+(defn process-opcode-and-modes
+  "Takes in a number and returns an opcode (the two rightmost digits), and a
+  list of parameter modes (the left digits, in reverse order). After the given
+  digits of the mode is an infinite lazy list of 0s.
+  `(process-opcode-and-modes 1002)` returns `[2 [0 1 0 0 ...]]`"
+  [n]
+  (let [opcode (mod n 100)
+        modes  (reverse (digits (int (/ n 100)) ))]
+    [opcode (concat modes (repeat 0))]))
 
 (defn intcode
   "The Intcode interpreter - takes a sequence of integers as input, and spits
   out the completed program state"
   ([program] (intcode program 0))
   ([program pc]
-   (let [opcode (nth program pc)]
+   (let [[opcode modes]  (process-opcode-and-modes (nth program pc))
+         read-param      (fn [n]
+                           (let [param (nth program (+ pc n))]
+                             (if (zero? (nth modes (dec n)))
+                               (nth program param)
+                               param)))
+         get-write-param (fn [n] (nth program (+ pc n)))
+         write-at-param  (fn [pos v] (assoc program (get-write-param pos) v))
+         binary-op       (fn [f]
+                           (write-at-param 3 (f (read-param 1) (read-param 2))))
+         jump-op         (fn [f]
+                           (if (f (read-param 1)) (read-param 2) (+ pc 3)))
+         comp-op         (fn [f] (binary-op #(if (f %1 %2) 1 0)))
+         input-from-user (fn [] (let [input (Integer/valueOf (read-line))]
+                                  (write-at-param 1 input)))
+         print-op        (fn [] (do (println (read-param 1)) program))]
      (case opcode
-       ; Addition
-       1  (recur (binary-op program pc +) (+ pc 4))
-       ; Multiplication
-       2  (recur (binary-op program pc *) (+ pc 4))
-       ; Read input
-       3  (let [input (Integer/valueOf (read-line))
-                program' (assoc program (nth program (inc pc)) input)]
-            (recur program' (+ pc 2)))
-       ; Print output
-       4 (do
-           (println (program (program (inc pc))))
-           (recur program (+ pc 2)))
-       ; Halt
-       99 program
+       1 (recur (binary-op +)     (+ pc 4))                     ; Add
+       2 (recur (binary-op *)     (+ pc 4))                     ; Mult
+       3 (recur (input-from-user) (+ pc 2))                     ; Read
+       4 (recur (print-op)        (+ pc 2))                     ; Print
+       5 (recur program           (jump-op (complement zero?))) ; Jump if true
+       6 (recur program           (jump-op zero?))              ; Jump if false
+       7 (recur (comp-op <)       (+ pc 4))                     ; Less than
+       8 (recur (comp-op =)       (+ pc 4))                     ; Equals
+       99 program                                               ; Halt
        :error))))
 
 (defn get-input
--- test/aoc2019/intcode_test.clj
+++ test/aoc2019/intcode_test.clj
@@ -15,3 +15,67 @@
   (testing "data space"
     (is (= (intcode [1 1 1 4 99 5 6 0 99]) [30 1 1 4 2 5 6 0 99])
         "You should be able to write into the space after a 99 command")))
+
+(deftest immediate-mode
+  (is (= (intcode [1101 1 2 5 99 0]) [1101 1 2 5 99 3]))
+  (are [in out] (= (intcode in) out)
+    [1101 1 2 5 99 0] [1101 1 2 5 99 3]
+    [1102 2 3 5 99 0] [1102 2 3 5 99 6]))
+
+(defn io-test [program in out]
+  (= out (with-out-str
+           (with-in-str in
+             (intcode program)))))
+
+(deftest reading
+  (= (with-in-str "8\n9" (intcode [3 6 3 7 99 0 0]))
+     [3 6 3 7 99 8 9]))
+
+(deftest printing
+  (= (with-out-str (intcode [4 0 4 4 99]))
+     "4\n99\n"))
+
+(deftest equals
+  (testing "Basic tests"
+    (is (= (intcode [8 0 1 5 99 -1])
+       [8 0 1 5 99 0]))
+    (is (= (intcode [1108 5 5 0 99]))
+        [1 5 5 0 99]))
+  (testing "Given examples"
+    (let [position-mode  [3 9 8 9 10 9 4 9 99 -1 8]
+          immediate-mode [3 3 1108 -1 8 3 4 3 99]]
+      (are [program in out] io-test
+        position-mode  "8" "1\n"
+        position-mode  "9" "0\n"
+        immediate-mode "8" "1\n"
+        immediate-mode "9" "0\n"))))
+
+(deftest less-than
+  (testing "Given examples"
+    (let [position-mode  [3 9 7 9 10 9 4 9 99 -1 8]
+          immediate-mode [3 3 1107 -1 8 3 4 3 99]]
+      (are [program in out] io-test
+        position-mode  "7" "1\n"
+        position-mode  "9" "0\n"
+        immediate-mode "7" "1\n"
+        immediate-mode "9" "0\n"))))
+
+; TODO Basic jumping tests
+
+(deftest jumping
+  (let [position-mode  [3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9]
+        immediate-mode [3 3 1105 -1 9 1101 0 0 12 4 12 99 1]]
+    (are [program in out] io-test
+      position-mode  "0"  "0\n"
+      position-mode  "77" "1\n"
+      immediate-mode "0"  "0\n"
+      immediate-mode "77" "0\n")))
+
+(deftest integration
+  (let [program [3 21 1008 21 8 20 1005 20 22 107 8 21 20 1006 20 31 1106 0 36
+                 98 0 0 1002 21 125 20 4 20 1105 1 46 104 999 1105 1 46 1101
+                 1000 1 20 4 20 1105 1 46 98 99]]
+    (are [in out] (io-test program in out)
+      "7" "999\n"
+      "8" "1000\n"
+      "9" "1001\n")))
