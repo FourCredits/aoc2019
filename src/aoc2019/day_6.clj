75322a1023e2fa4911ca7cfcea946a18f46fff58
Erik <erik.grundy@gmail.com>
2021-07-16 01:11:30 +1200

    Implement part 2

--- src/aoc2019/day_6.clj
+++ src/aoc2019/day_6.clj
@@ -24,7 +24,26 @@
           (recur (+ result depth)
                  (apply list (concat (pop queue) (map #(vector (inc depth) %) (orbits k)))))))))
 
-(defn get-input [] nil)
+(defn find-parent [object orbits]
+  (first (first (filter #(some #{object} (second %)) orbits))))
+
+(defn find-parents [object orbits]
+  (let [parent (find-parent object orbits)]
+    (if (nil? parent)
+      '()
+      (cons parent (find-parents parent orbits)))))
+
+(defn num-orbital-transfers [orbits source target]
+  (let [source-parents (find-parents source orbits)
+        target-parents (find-parents target orbits)
+        common-parent  (first
+                         (filter #(some #{%} target-parents) source-parents))]
+    (+ (.indexOf source-parents common-parent)
+       (.indexOf target-parents common-parent))))
+
+(defn part-2 [orbits]
+  (num-orbital-transfers orbits "YOU" "SAN"))
+
 (defn parse [string]
   (make-table (map #(str/split % #"\)") (str/split-lines string))))
 
