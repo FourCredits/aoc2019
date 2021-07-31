e81c26741a1e309c3108576dcd7c3c8203c0aa04
Erik <erik.grundy@gmail.com>
2021-08-01 02:05:49 +1200

    Reformat core file

--- src/aoc2019/core.clj
+++ src/aoc2019/core.clj
@@ -1,26 +1,26 @@
 (ns aoc2019.core
   (:gen-class)
-  (:require [aoc2019.day-1 :as d1]
-            [aoc2019.day-2 :as d2]
-            [aoc2019.day-3 :as d3]
-            [aoc2019.day-4 :as d4]
-            [aoc2019.day-5 :as d5]
-            [aoc2019.day-6 :as d6]
-            [aoc2019.day-7 :as d7]
-            [aoc2019.day-8 :as d8]
-            [aoc2019.day-9 :as d9]
+  (:require [aoc2019.day-1  :as d1]
+            [aoc2019.day-2  :as d2]
+            [aoc2019.day-3  :as d3]
+            [aoc2019.day-4  :as d4]
+            [aoc2019.day-5  :as d5]
+            [aoc2019.day-6  :as d6]
+            [aoc2019.day-7  :as d7]
+            [aoc2019.day-8  :as d8]
+            [aoc2019.day-9  :as d9]
             [aoc2019.day-10 :as d10]))
 
 (def main-funcs
-  {1 d1/-main
-   2 d2/-main
-   3 d3/-main
-   4 d4/-main
-   5 d5/-main
-   6 d6/-main
-   7 d7/-main
-   8 d8/-main
-   9 d9/-main
+  { 1  d1/-main
+    2  d2/-main
+    3  d3/-main
+    4  d4/-main
+    5  d5/-main
+    6  d6/-main
+    7  d7/-main
+    8  d8/-main
+    9  d9/-main
    10 d10/-main})
 
 (defn -main
