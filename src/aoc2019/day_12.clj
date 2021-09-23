8d8e4cda9e007007d089cb6b77d9b05ecfc4a3bf
Erik <erik.grundy@gmail.com>
2021-09-24 01:13:08 +1200

    Removed unncessary function

--- src/aoc2019/day_12.clj
+++ src/aoc2019/day_12.clj
@@ -40,29 +40,6 @@
 
 ; Part 2
 
-; Each axis is independent of each other, so each can be treated independently
-; You find the repeat point of each of the three axes, then take the lcm of them
-
-(defn axis-equal [axis moons1 moons2]
-  (every? (fn [pair] (apply = (map #(nth (:pos %) axis) pair)))
-          (map vector moons1 moons2)))
-
-(comment
-
-(def starting-positions-1
-  [{:pos [-1   0  2] :vel [0 0 0]}
-   {:pos [ 2 -10 -7] :vel [0 0 0]}
-   {:pos [ 4  -8  8] :vel [0 0 0]}
-   {:pos [ 3   5 -1] :vel [0 0 0]}])
-
-(def starting-positions-2
-  [{:pos [-8 -10  0] :vel [0 0 0]}
-   {:pos [ 5   5 10] :vel [0 0 0]}
-   {:pos [ 2  -7  3] :vel [0 0 0]}
-   {:pos [ 9  -8 -3] :vel [0 0 0]}])
-
-  )
-
 (defn get-axis
   "Takes the state of the system, an axis to find, and returns a collection of
   the positions and velocities of each moon at that axis"
